ccd73415202bdea21c767481e02e2a6c3ef6611e
titoCo12 <joaquinfuentes170@gmail.com>
2025-12-06 00:28:32 -0300

    endpoint buscar por generos

--- backend/index.js
+++ backend/index.js
@@ -24,21 +24,22 @@ function leerPelis() {
     }
 }
 
-// ENDPOINTS ---------------------------
 
-//Endpoint base - GET
+// ENDPOINTS GET ---------------------------
+
+//Endpoint base 
 app.get('/', (req, res) => {
     res.json({
-        titulo: "🎬 Herbert Flix",
-        endpoints: [
-            'GET /api/pelis', 
-            'GET /api/pelis/']
-    })
+    titulo: "🎬 Herbert Flix",
+    endpoints: [
+        'GET /api/pelis',
+        'GET /api/pelis/']
+    });
 });
 
 
-//Endpoint todas las pelis del usuario - GET
-app.get('/api/pelis', (req, res) => {
+//Endpoint todas las pelis del usuario
+app.get('/api/mis-pelis', (req, res) => {
   const pelis = leerPelis();
   res.json({
     mensaje: 'Peliculas del usuario',
@@ -48,11 +49,11 @@ app.get('/api/pelis', (req, res) => {
 });
 
  
-//Endpoint pelis por titulo - GET
-app.get('/api/pelis/busqueda-titulo/:titulo', (req, res) => {
-    
+//Endpoint pelis por titulo 
+app.get('/api/mis-pelis/busqueda/titulo/:titulo', (req, res) => {  
     const titulo_pedido = req.params.titulo;
     const titulo_lower = titulo_pedido.toLowerCase();
+
     const pelis = leerPelis().filter(p => 
         p.titulo.toLowerCase()       // no es case sensitive
         .replace(/[^a-z0-9]/gi, '')  // solo considero alfanumericos
@@ -67,6 +68,35 @@ app.get('/api/pelis/busqueda-titulo/:titulo', (req, res) => {
 
 
 //Endpoint pelis por genero
+//Recibe un string con la forma "gen1, gen2.." de parametro
+app.get('/api/mis-pelis/busqueda/generos/:generos', (req, res) => {
+    try {
+        const generos = req.params.generos
+            .split(",")
+            .map(g => decodeURIComponent(g) //atajo acentos
+                .trim()                     //saco espacios
+                .toLowerCase());           
+
+        const pelis = leerPelis().filter(p => 
+            p.generos && generos.every(gen =>         
+                p.generos.map(g => g.toLowerCase()).includes(gen) 
+            ));
+
+        res.json({
+            generos: generos,
+            cant_resultados: pelis.length,
+            resultados: pelis
+        })
+    }
+
+    catch (error) {
+        res.status(400).json({ error: "Formato de Generos no valido"})
+    }
+});
+
+
+
+
 
 // INICIO ------------------------------
 
