f6512ddc3d9de31c1b23274175023a0a9addaef1
titoCo12 <joaquinfuentes170@gmail.com>
2025-12-07 13:30:46 -0300

    refactor y endpoint delete visualizaciones

--- backend/index.js
+++ backend/index.js
@@ -76,16 +76,22 @@ function ordenarPelis(orden, ordenar_por, pelis) {
     });
 }
 
+
 // ENDPOINTS GET ---------------------------
 
-
 //Endpoint base 
+// valores de filtro watchlist: true - false
+// valores de filtro ordenar_por: anio - rating_personal - fecha_visto
+// valores de ordenamiento orden: asc - desc
 app.get('/', (req, res) => {
     res.json({
-    titulo: "🎬 Herbert Flix",
-    endpoints: [
-        'GET /api/pelis',
-        'GET /api/pelis/']
+    titulo: "🎬 Herbert Flix 🎬",
+    endpoints: { 
+        ver_todas_las_pelis: 'GET /api/mis-pelis',
+        ver_watchlist: 'GET /api/mis-pelis?watchlist=true',
+        buscar_por_texto_watchlist_orden: 'GET /api/mis-pelis?q=<texto>&ordenar_por=<valor>&orden=<asc-desc>&watchlist=<true-false>',
+        buscar_peli_por_id: 'GET /api/mis-pelis/:id',
+        agregar_peli: 'POST /api/mis-pelis'}
     });
 });
 
@@ -121,6 +127,31 @@ app.get('/api/mis-pelis', (req, res) => {
         
     } catch (error) {
         console.error('Error en GET /api/mis-pelis:', error.message);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+//Endpoint peli por id (dev)
+app.get('/api/mis-pelis/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+        const peli = leerPelis().find(p => p.id == id);
+        
+        if (!peli) {
+            return res.status(404).json({ 
+                error: 'visualizacion de peli no encontrada',
+                id_buscado: id,
+            });
+        }
+        
+        res.json({
+            mensaje: 'Visualización encontrada',
+            pelicula: peli
+        });
+        
+    } catch (error) {
+        console.error('Error en GET /api/mis-pelis/:id:', error.message);
         res.status(500).json({ error: error.message });
     }
 });
@@ -175,6 +206,48 @@ app.post('/api/mis-pelis', (req,res) => {
     
 })
 
+
+//ENDPOINTS DELETE ---------------------
+
+//Endpoint eliminar visualizaciones de una pelicula
+app.delete('/api/mis-pelis/des-ver/:imdb_id', (req, res) => {
+    try {
+        const imdb_id = req.params.imdb_id;
+        
+        let pelis = leerPelis();
+        const total = pelis.length;
+    
+        const pelisEliminadas = pelis.filter(p => p.imdb_id === imdb_id);
+        const pelisFinal = pelis.filter(p => p.imdb_id !== imdb_id);
+        
+        // si no se elimino nada no reescribo el json
+        if (pelisEliminadas.length == 0) {
+            return res.status(404).json({ 
+                mensaje: 'No se encontraron regisgtros para eliminar',
+                imdb_id: imdb_id
+            });
+        }
+        
+        // reescribo
+        const ruta = path.join(__dirname, "localdata", "pelis.json");
+        const datos = JSON.stringify(pelisFinal, null, 2);
+        fs.writeFileSync(ruta, datos, 'utf-8');
+        
+        // Respuesta exitosa
+        res.json({
+            estado: `se eliminaron registros de la peli ${imdb_id}`,
+            titulo: pelisEliminadas[0].titulo,
+            detalles: {
+                registros_borrados: pelisEliminadas.length,
+                restantes: pelisFinal.length,
+            }
+        });
+        
+    } catch (error) {
+        console.error('Error en DELETE /api/mis-pelis/imdb/:imdb_id:', error.message);
+        res.status(500).json({ error: error.message });
+    }
+});
 
 // INICIO ------------------------------
 
